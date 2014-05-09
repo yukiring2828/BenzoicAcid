58cae671fb9a52048a6e86939c5debf7d19a2c3a
g12908hm <g12908hm+git@s30419.tsuda.ac.jp>
2014-05-09 14:33:29 +0900

    10まんまでおわったよ！

--- Kadai5_1/src/kadai/IntToEng.java
+++ Kadai5_1/src/kadai/IntToEng.java
@@ -13,70 +13,54 @@ public class IntToEng {
 		int cpy=n;
 		boolean flag=false;
 		if(n==0)flag=true;
-		int[]a=new int[5];
-		int i=10000,count=0;
+		int[]a=new int[6];
+		int i=100000,count=0;
 		while(i>0){
 			a[count]=n/i;
 			n%=i;
 			i/=10;
 			count++;
 		}
-		int x=a[4]+a[3]*10;
-		int y=a[0]+a[1]*10;
 		String num="";
-		/*
-		if(y>19 && a[1]!=0)num+=oneplace(a[1])+"thausand";//20à»è„ÇÃ1ÇÃà 
-		if(y<20&&y>0)num+=oneplace(y)+"thausand";
-		if(a[2]!=0)num=num+oneplace(a[2])+"hundred";
-		if(x>19)num+=tenplace(a[3]);
-		if(x>19 && a[4]!=0)num+=oneplace(a[4]);//20à»è„ÇÃ1ÇÃà 
-		if(x<20&&x>0)num+=oneplace(x);//20à»â∫ÇÃÇ∆Ç´ÇÃèàóù
-		*/
-		num=marugoto(a[2],a[3],a[4]);
-		if(flag)num="zero";
+		num=marugoto(a[0],a[1],a[2]);
+		if(a[0]+a[1]+a[2]!=0)num+="thousand ";
+		num+=marugoto(a[3],a[4],a[5]);
+		if(flag)num="zero ";
 		return num ;
 	}
 	static String marugoto(int a,int b,int c){
-		
-	String num="";
-		
+		if(a+b+c==0)return "";
+		String num="";
 		int x=b*10+c;
-		
-		if(a!=0)num=num+oneplace(a)+"hundred";
+		if(a!=0)num=num+oneplace(a)+"hundred ";
 		if(x>19)num+=tenplace(b);
 		if(x>19&&c!=0)num+=oneplace(c);
 		if(x<20&&x>0)num+=oneplace(x);
 		return num;
-		/*
-		if(n/100!=0){num+=oneplace(n/100)+"hundred";
-		n=n%100;
-		}
-		if(n>19)num+=tenplace(n);
-		*/
 	}
 	
 	static String oneplace(int n){
 		String numbers[]={
-				"zero",
-				"one",
-				"two",
-				"three",
-				"four",
-				"five",
-				"six",
-				"seven",
-				"eight",
-				"nine",
-				"ten",
-				"eleven",
-				"twelve",
-				"thrirteen",
-				"fourteen",
-				"fifteen",
-				"sixteen",
-				"seventeen",
-				"eighteen",
-				"nineteen"
+				"zero ",
+				"one ",
+				"two ",
+				"three ",
+				"four ",
+				"five ",
+				"six ",
+				"seven ",
+				"eight ",
+				"nine ",
+				"ten ",
+				"eleven ",
+				"twelve ",
+				"thrirteen ",
+				"fourteen ",
+				"fifteen ",
+				"sixteen ",
+				"seventeen ",
+				"eighteen ",
+				"nineteen "
 		};
 		return numbers[n];
 	}
@@ -84,14 +68,14 @@ public class IntToEng {
 	static String tenplace(int n){
 		n=n-2;
 		String numbers[]={
-				"twenty",
-				"thirty",
-				"forty",
-				"fifty",
-				"sixty",
-				"seventy",
-				"eighty",
-				"ninety",
+				"twenty ",
+				"thirty ",
+				"forty ",
+				"fifty ",
+				"sixty ",
+				"seventy ",
+				"eighty ",
+				"ninety ",
 				
 		};
 		return numbers[n];
--- Kadai5_1/test/kadai/IntToEngTest.java
+++ Kadai5_1/test/kadai/IntToEngTest.java
@@ -10,31 +10,48 @@ public class IntToEngTest {
 	@Test
 	public void oneplace() {
 		IntToEng com=new IntToEng();
-		String expect="eighteen";
+		String expect="eighteen ";
 		String answer=com.oneplace(18);
+		assertThat(answer,is(expect));
+	}
+	
+	@Test
+	public void tenplace() {
+		IntToEng com=new IntToEng();
+		String expect="twenty ";
+		String answer=com.tenplace(2);
+		assertThat(answer,is(expect));
+	}
+	
+	@Test
+	public void marugoto() {
+		IntToEng com=new IntToEng();
+		String expect="five hundred nineteen ";
+		String answer=com.marugoto(5,1,9);
 		assertThat(answer,is(expect));
 	}
 	
 	@Test
 	public void translateEng() {
 		IntToEng com=new IntToEng();
-		String expect="twenty";
-		String expect2="thirtytwo";
-		String expect3="onehundred";
-		String expect4="onehundredeleven";
-		String expect5="onethousand";
-		String expect6="onethousandnineteen";
-		String expect7="onethousandtwenty";
-		String expect8="twothousandthirtytwo";
-		String expect9="twothousandonehundred";
-		String expect10="tenthousand";
-		String expect11="tenthousandelvenonehandredeleven";
-		String expect12="tenthousandelvenonehandredtwenty";
-		String answer=com.translateEng(20);
+		String expect="zero ";
+		String expect2="thirty two ";
+		String expect3="one hundred ";
+		String expect4="one hundred eleven ";
+		String expect5="one thousand ";
+		String expect6="one thousand nineteen ";
+		String expect7="one thousand twenty ";
+		String expect8="two thousand thirty two ";
+		String expect9="two thousand one hundred ";
+		String expect10="ten thousand ";
+		String expect11="eleven thousand one hundred eleven ";
+		String expect12="eleven thousand one hundred twenty ";
+		String expect13="eight hundred eighty eight thousand eight hundred eighty eight ";
+
+		String answer=com.translateEng(0);
 		String answer2=com.translateEng(32);
 		String answer3=com.translateEng(100);
 		String answer4=com.translateEng(111);
-		/*
 		String answer5=com.translateEng(1000);
 		String answer6=com.translateEng(1019);
 		String answer7=com.translateEng(1020);
@@ -43,12 +60,13 @@ public class IntToEngTest {
 		String answer10=com.translateEng(10000);
 		String answer11=com.translateEng(11111);
 		String answer12=com.translateEng(11120);
-		*/
+		String answer13=com.translateEng(888888);
+		
 		assertThat(answer,is(expect));
 		assertThat(answer2,is(expect2));
 		assertThat(answer3,is(expect3));
 		assertThat(answer4,is(expect4));
-		/*
+		
 		assertThat(answer5,is(expect5));
 		assertThat(answer6,is(expect6));
 		assertThat(answer7,is(expect7));
@@ -57,7 +75,8 @@ public class IntToEngTest {
 		assertThat(answer10,is(expect10));
 		assertThat(answer11,is(expect11));
 		assertThat(answer12,is(expect12));
-		*/
+		assertThat(answer13,is(expect13));
+		
 	}
 
 
