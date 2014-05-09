cedf81806ac982737a05cf26e46c3a766d662f16
g12941of <g12941of+git@s30420.tsuda.ac.jp>
2014-05-09 12:09:32 +0900

    driver 10の位

--- Kadai5_1/test/kadai/IntToEngTest.java
+++ Kadai5_1/test/kadai/IntToEngTest.java
@@ -10,9 +10,21 @@ public class IntToEngTest {
 	@Test
 	public void oneplace() {
 		IntToEng com=new IntToEng();
-		String expect="zero";
-		String answer=com.oneplace(0);
+		String expect="eighteen";
+		String answer=com.oneplace(18);
 		assertThat(answer,is(expect));
 	}
+	
+	@Test
+	public void translateEng() {
+		IntToEng com=new IntToEng();
+		String expect="twenty";
+		String expect2="thirtyfour";
+		String answer=com.translateEng(20);
+		String answer2=com.translateEng(34);
+		assertThat(answer,is(expect));
+		assertThat(answer2,is(expect2));
+	}
+
 
 }
